f8e33d7b2f3e3db144352c7293060fab997aa667
Yorqinjon Mahmudov <yorqinjonmahmudov93@gmail.com>
2022-08-06 17:23:38 +0000

    Update ElectronicWatch.java

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -7,6 +7,12 @@ public class ElectronicWatch {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
 
+        seconds %= 86400;
+        int second = seconds%60;
+        int minutes = seconds %3600 /60;
+        int hours = seconds /3600;
+        DecimalFormat df = new DecimalFormat("00");
+        System.out.println(hours+":"+df.format(minutes)+":"+df.format(second));
 
     }
 }
